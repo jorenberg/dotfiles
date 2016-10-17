a7fc322a95642664d15a1b5ab770f5a55b7ff10e
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-10-17 22:22:42 +0530

    Initial commit
    To check Git is available or not!

--- git/setup.sh
+++ git/setup.sh
@@ -6,3 +6,17 @@
 # Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
 # Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
 # ====================================================================
+
+IFS=:
+GIT=false
+for d in $PATH
+do test -x $d/git && GIT=true
+done
+if $GIT
+then
+  echo "git found!"
+  git config --global user.name "iamprabhat"
+  git config --global user.email "*********@gmail.com"
+  git config --global core.excludesfile "$HOME/.gitignore"
+else echo "no git"
+fi
