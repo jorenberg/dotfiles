08a433b75c27a7729c191061d774664ec8ad6bb2
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-03 17:24:05 +0530

    Initial commit
    A hook script to check the commit log message taken by "applypatch" from an e-mail message.

--- git/msg-applypatch.sh
+++ git/msg-applypatch.sh
@@ -6,3 +6,14 @@
 # Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
 # Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
 # ====================================================================
+
+# An example hook script to check the commit log message taken by
+# applypatch from an e-mail message.
+#
+# The hook should exit with non-zero status after issuing an
+# appropriate message if it wants to stop the commit.
+# The hook is allowed to edit the commit message file.
+. git-sh-setup
+test -x "$GIT_DIR/hooks/commit-msg" &&
+  exec "$GIT_DIR/hooks/commit-msg" ${1+"$@"}
+:
